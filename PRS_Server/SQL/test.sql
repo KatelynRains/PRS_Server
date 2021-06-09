ffd6ea37024529881ab7735e2ce53b569c7ce566
Katelyn Rains <KRains@tql.com>
2021-06-09 08:00:37 -0400

    added sql stuff

---
+++ PRS_Server/SQL/test.sql
@@ -0,0 +1,16 @@
+﻿use PrsDb;
+--user data
+INSERT into Users (Username, Password, Firstname, Lastname, Phone, Email, IsReviewer, IsAdmin)
+
+VALUES('krains', 'password1','Katelyn', 'Rains', '952-738-1597', 'krains@uwalumni.com', 1, 1);
+go
+ 
+Insert into Vendors (Code, Name, Address, City, State, Zip, PHone, Email)
+VALUES('AMAZ', 'Amazon', '1 Amazon Way', 'Seattle', 'WA', '911', 'info@amazon.com');
+go
+
+declare @amazon varchar(10);
+Select @amazon = Id from Vendors where Code = 'AMAZ';
+Insert into Products (PartNbr, Name, Price, Unit, Photopath, VendorId)
+VALUES ('ECHO', 'Amazon Echo', 100, 'Each', null, @amazon);
+go
